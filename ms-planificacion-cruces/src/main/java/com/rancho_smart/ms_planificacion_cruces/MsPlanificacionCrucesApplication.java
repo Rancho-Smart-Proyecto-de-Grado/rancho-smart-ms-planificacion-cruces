e3e2ea49b32c61143df3dd3a3120eccc9a799dea
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 03:01:35 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ ms-planificacion-cruces/src/main/java/com/rancho_smart/ms_planificacion_cruces/MsPlanificacionCrucesApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_planificacion_cruces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPlanificacionCrucesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPlanificacionCrucesApplication.class, args);
+	}
+
+}
---
+++ ms-planificacion-cruces/src/test/java/com/rancho_smart/ms_planificacion_cruces/MsPlanificacionCrucesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_planificacion_cruces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPlanificacionCrucesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
